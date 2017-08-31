8cebe70e7ab1a29d1ccfc4bc1d36aaf2ccd00b75
ChengYien <cye_orange@163.com>
2017-08-31 17:04:41 +0800

    a

--- js/index.js
+++ js/index.js
@@ -2,48 +2,56 @@
 * @Author: Marte
 * @Date:   2017-05-26 11:03:33
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-08-31 13:40:49
+* @Last Modified time: 2017-08-31 16:56:29
 */
 
 $(document).ready(function(){
-    // 滚动条
-    $('body').swipeUp(function () {
-        $('.tip-top').css({
-            top: '0'
+    // 滚动条顶部吸附效果
+    (function () {
+        var thisTop = $('.tip-top').offset().top;
+        $(window).on('scroll', function(event) {
+            var py = $(window).scrollTop();
+            if (py >= thisTop) {
+                $('.tip-top').css({
+                    'position': 'fixed',
+                    'top': '0'
+                });
+            } else {
+                $('.tip-top').css({
+                    'position': 'absolute',
+                    'top': thisTop + 'px'
+                });
+            }
         });
-    });
-    $('body').swipeDown(function () {
-        $('.tip-top').css({
-            top: '0.1rem'
-        });
-    });
-
+    })();
+    
+    
+    
     // 注意
     // 后台返回数据时插入DOM调用此方法可实现滚动
     // 单个元素分别调用
-    function scrollBar(eleMsg) {
-        function initBarMsg($ele) {
-            $ele.css({
-                right: -$ele.width() + 'px'
-            });
+    (function () {
+        function scrollBar(eleMsg) {
+            function initBarMsg($ele) {
+                $ele.css({
+                    right: -$ele.width() + 'px'
+                });
+            }
+
+            function scrollBarMsg($ele) {
+                var px = -$('.tip-top').width()-$ele.width();
+                $ele.animate({
+                    'transform': 'translateX(' + px + 'px)',
+                    '-webkit-transform': 'translateX(' + px + 'px)',
+                }, 10000, function () {
+                    $ele.remove();
+                })
+            }
+            initBarMsg(eleMsg);
+            scrollBarMsg(eleMsg);
         }
-
-        function scrollBarMsg($ele) {
-            var px = -$('.tip-top').width()-$ele.width();
-            $ele.animate({
-                'transform': 'translateX(' + px + 'px)',
-                '-webkit-transform': 'translateX(' + px + 'px)',
-            }, 10000, function () {
-                $ele.remove();
-            })
-        }
-        initBarMsg(eleMsg);
-        scrollBarMsg(eleMsg);
-    }
-    scrollBar($('.tip-top p'))
-
-
-
+        scrollBar($('.tip-top p'))
+    })()
 
     // 关闭弹窗
     $('.pop .btn').on('tap', function(event) {
@@ -55,6 +63,9 @@ $(document).ready(function(){
         $('.mask').hide();
     });
 
+    $('.pop').on('touchmove', function (event) {
+        event.preventDefault();
+    });
     // 卡片翻转计数
     var rotateConut = 0;
     // 卡片翻转
